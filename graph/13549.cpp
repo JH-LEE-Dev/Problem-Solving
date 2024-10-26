4b124c8918f1151a3a63deb16b15063f68cfd8cd
StrongBiceps <140565804+StrongBiceps@users.noreply.github.com>
2024-10-26 16:53:40 +0900

    Updated 2024.10.26

---
+++ graph/13549.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+
+#define endl '\n'
+using namespace std;
+
+#define MAX 100'000
+int N{ 0 }, K{ 0 };
+queue<int> myQueue;
+vector<bool> visited(MAX + 5, false);
+int counting{ 0 };
+
+vector<int> dist(MAX + 5, numeric_limits<int>::max());
+
+void bfs()
+{
+    myQueue.push(N);
+
+    while (!myQueue.empty())
+    {
+        int current_vertex{ myQueue.front() };
+        myQueue.pop();
+
+        visited[current_vertex] = true;
+
+        if (current_vertex - 1 >= 0 && dist[current_vertex] + 1 < dist[current_vertex - 1])
+        {
+            myQueue.push(current_vertex - 1);
+            dist[current_vertex - 1] = min(dist[current_vertex - 1], dist[current_vertex] + 1);
+        }
+
+        if (current_vertex + 1 <= MAX && dist[current_vertex] + 1 < dist[current_vertex + 1])
+        {
+            myQueue.push(current_vertex + 1);
+            dist[current_vertex + 1] = min(dist[current_vertex + 1], dist[current_vertex] + 1);
+        }
+
+        if (current_vertex * 2 <= MAX && dist[current_vertex * 2] > dist[current_vertex])
+        {
+            myQueue.push(current_vertex * 2);
+            dist[current_vertex * 2] = dist[current_vertex];
+        }
+    }
+}
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> N >> K;
+
+    dist[N] = 0;
+    bfs();
+
+    cout << dist[K];
+
+    return 0;
+}
