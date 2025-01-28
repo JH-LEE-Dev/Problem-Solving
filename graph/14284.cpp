f70547f6cc8b498ca001d8321aaa6b7180ba679b
StrongBiceps <140565804+StrongBiceps@users.noreply.github.com>
2025-01-28 13:26:26 +0900

    Updated 2025.01.28

---
+++ graph/14284.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <limits>
+#include <tuple>
+#include <string>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+
+using namespace std;
+using vertex = tuple<int, int>;
+
+#define MAX 5'001
+#define endl '\n'
+#define INF numeric_limits<int>::max()
+
+int N{ 0 }, M{ 0 };
+vector<vector<pair<int, int>>> edges(MAX, vector<pair<int, int>>());
+vector<int> dist(MAX, INF);
+int start{ 0 }, goal{ 0 };
+
+void input()
+{
+	cin >> N >> M;
+
+	for (int i{ 0 }; i < M; ++i)
+	{
+		int src{ 0 }, dst{ 0 }, w{ 0 };
+
+		cin >> src >> dst >> w;
+
+		edges[src].push_back({ dst,w });
+		edges[dst].push_back({ src,w });
+	}
+
+	cin >> start >> goal;
+}
+
+void dijkstra()
+{
+	dist[start] = 0;
+	priority_queue<vertex, vector<vertex>, greater<vertex>> pq;
+	pq.push({ 0,start });
+
+	while (pq.empty() == false)
+	{
+		auto cur{ pq.top() };
+		pq.pop();
+
+		auto src{ get<1>(cur) };
+		auto curDist{ get<0>(cur) };
+
+		if (dist[src] < curDist)
+			continue;
+
+		if (src == goal)
+			break;
+
+		for (auto& edge : edges[src])
+		{
+			auto dst{ edge.first };
+			auto w{ edge.second };
+			auto newDist{ curDist + w };
+
+			if (dist[dst] > newDist)
+			{
+				dist[dst] = newDist;
+				pq.push({ newDist,dst });
+			}
+		}
+	}
+}
+
+void solve()
+{
+	dijkstra();
+	cout << dist[goal];
+}
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	input();
+	solve();
+
+	return 0;
+}
